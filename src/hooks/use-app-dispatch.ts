5f79672dea061d4edab931ecc0edbf57209b4fde
artemmatiushenko1 <artom.matyushenko@gmail.com>
2022-05-21 21:45:03 +0300

    Created typed useSelector and useDispatch hooks

---
+++ src/hooks/use-app-dispatch.ts
@@ -0,0 +1,6 @@
+import { AppDispatch } from 'features/store';
+import { useDispatch } from 'react-redux';
+
+const useAppDispatch = () => useDispatch<AppDispatch>();
+
+export default useAppDispatch;
