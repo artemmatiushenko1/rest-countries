b10da9b13c6a21002afa59a6aab4bc5ae2036361
artemmatiushenko1 <artom.matyushenko@gmail.com>
2022-10-04 22:31:24 +0300

    Made title text as a link to homepage

--- src/components/header/Header.style.ts
+++ src/components/header/Header.style.ts
@@ -1,4 +1,4 @@
-import { styled } from '@mui/system';
+import { styled, lighten } from '@mui/system';
 import { AppBar as MuiAppBar, Toolbar as MuiToolbar } from '@mui/material';
 
 export const AppBar = styled(MuiAppBar)(({ theme }) => {
@@ -11,12 +11,22 @@ export const AppBar = styled(MuiAppBar)(({ theme }) => {
   };
 });
 
-export const Toolbar = styled(MuiToolbar)(() => {
+export const Toolbar = styled(MuiToolbar)(({ theme }) => {
   return {
     justifyContent: 'space-between',
     '&.MuiToolbar-root': {
       padding: '0',
       minHeight: '80px',
+    },
+    a: {
+      textDecoration: 'none',
+      transition: 'color 0.2s ease',
+      '&:link, &:visited, &:active': {
+        color: theme.palette.text.primary,
+      },
+      '&:hover': {
+        color: lighten(theme.palette.text.primary, 0.3),
+      },
     },
   };
 });
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,16 @@
 import ThemeSwitch from 'components/theme-switch/ThemeSwitch';
 import Container from 'components/container/Container';
 import * as S from './Header.style';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <S.AppBar position="relative">
       <Container>
         <S.Toolbar>
-          <S.Title>Where in the world?</S.Title>
+          <Link to="/">
+            <S.Title>Where in the world?</S.Title>
+          </Link>
           <ThemeSwitch />
         </S.Toolbar>
       </Container>
